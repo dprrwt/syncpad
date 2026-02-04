fefdb3b2d36a6762ad48913f610fb19240689dfa
dprrwt <dprrawat296@gmail.com>
2026-02-04 21:37:11 +0530

    fix: update signaling servers + add STUN for NAT traversal

--- src/components/EditorRoom.tsx
+++ src/components/EditorRoom.tsx
@@ -59,7 +59,22 @@ export default function EditorRoom({ roomId, onLeave }: EditorRoomProps) {
     ydocRef.current = ydoc
 
     const provider = new WebrtcProvider(`syncpad-${roomId}`, ydoc, {
-      signaling: ['wss://signaling.yjs.dev', 'wss://y-webrtc-signaling-eu.herokuapp.com', 'wss://y-webrtc-signaling-us.herokuapp.com'],
+      signaling: [
+        'wss://signaling.yjs.dev',
+        'wss://y-webrtc-ckynr.ondigitalocean.app',
+        'wss://signaling.yjs.io'
+      ],
+      // Add STUN servers for better NAT traversal
+      peerOpts: {
+        config: {
+          iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'stun:stun1.l.google.com:19302' },
+            { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:global.stun.twilio.com:3478' }
+          ]
+        }
+      }
     })
     providerRef.current = provider
 
